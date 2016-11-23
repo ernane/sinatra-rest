45310871d8d2320e18ff2ef80993c3cfe0d52221
Ernane Sena <ernane.sena@gmail.com>
2016-11-23 17:54:29 -0300

    CRUD completo

--- app/controllers/dashboard.rb
+++ app/controllers/dashboard.rb
@@ -8,17 +8,25 @@ get '/' do
   users.to_json
 end
 
-get '/users/:id' do
-  user = User.find_by_id(params[:id])
-  user.to_json
-end
-
 post "/users" do
   data = JSON.parse(request.body.read)
   user = User.create(data)
   #binding.pry
   redirect '/'
 end
+
+get '/users/:id' do
+  user = User.find_by_id(params[:id])
+  user.to_json
+end
+
+put '/users/:id' do
+  data = JSON.parse(request.body.read)
+	user = User.find_by_id(params[:id])
+  user.update(name: data[0]['name'], email: data[0]['email'])
+	redirect '/'
+end
+
 
 delete '/users/:id' do
 	user = User.find_by_id(params[:id])
